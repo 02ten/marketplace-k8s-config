43dd1818ad553208cfe10e31717fc42c86c84dac
Ten Aleksandr Aleksandrovich <89169604848t@gmail.com>
2024-04-15 15:58:38 +0300

    Changed init.sql

--- application/postgreSQL/init.sql
+++ application/postgreSQL/init.sql
@@ -44,3 +44,61 @@ create table t_product
         constraint fkp17nkwpqnnxh5iax87dc58sp3
             references t_category
 );
+create table t_order
+(
+    id            bigserial
+        primary key,
+    active        boolean          not null,
+    address       varchar(255),
+    creation_date timestamp(6),
+    payment       varchar(255),
+    summary       double precision not null,
+    user_id       bigint
+);
+create table t_cart
+(
+    id         bigserial
+        primary key,
+    quantity   bigint,
+    user_id    bigint,
+    product_id bigint
+        constraint fk71g7tegrdc2b7aamcmmof637x
+            references t_product
+);
+create table t_order_products
+(
+    id                bigserial
+        primary key,
+    quantity          bigint,
+    user_id           bigint,
+    product_id        bigint
+        constraint fkag13gits5n58hfq3cv07qyk81
+            references t_product,
+    order_products_id bigint
+        constraint fk6nne2jgo1jhov823i8tbt1a18
+            references t_order
+);
+create table t_images
+(
+    id                 bigserial
+        primary key,
+    bytes              bytea,
+    content_type       varchar(255),
+    is_preview_image   boolean not null,
+    original_file_name varchar(255),
+    size               bigint,
+    product_id         bigint
+        constraint fk2ahxt5kdje6xp5suofqmg2tsu
+            references t_product
+);
+create table t_product_images
+(
+    product_id bigint not null
+        constraint fkos4qfdy8f40cp5bch2jxceyw4
+            references t_product,
+    images_id  bigint not null
+        constraint uk_dw7hps4yi7vkr5q0ywek68dfv
+            unique
+        constraint fk30fedemxp63e2c3p62xjm8il9
+            references t_images
+);
